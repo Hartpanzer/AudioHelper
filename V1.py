cbf0334cd96a8ad885132500e1931dd352e79f7c
Sergey Potapov <hartpanzer@gmail.com>
2019-08-19 02:19:54 +0300

    E

--- V1.py
+++ V1.py
@@ -2,8 +2,8 @@ import pyaudio
 import speech_recognition as sr
 
 r=sr.Recognizer()
-r.energy_threshold = 4000  
-r.dynamic_energy_threshold = True  
+r.energy_threshold = 4000  #Начальный уровень шума
+r.dynamic_energy_threshold = True  #Динамический уровень шума
 print("Скажите что-нибудь...")
 with sr.Microphone() as source:
    audio=r.listen(source)
